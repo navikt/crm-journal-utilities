9ceaf8c03999c95a97d638be82fe011c6d53d78e
annolse <ann.kristin.olsen@nav.no>
2023-06-13 10:38:38 +0200

    fix

--- force-app/main/default/lwc/crmPersonCaseOverview/crmPersonCaseOverview.js
+++ force-app/main/default/lwc/crmPersonCaseOverview/crmPersonCaseOverview.js
@@ -135,12 +135,21 @@ export default class NksPersonCaseOverview extends LightningElement {
         let caseGroups = [];
 
         cases.forEach(caseItem => {
+            if(this.prefilledTheme === null || this.prefilledTheme===''){
                 if (groupedCases.hasOwnProperty(caseItem.themeName)) {
                     groupedCases[caseItem.themeName].push(caseItem);
                 } else {
                     groupedCases[caseItem.themeName] = [];
                     groupedCases[caseItem.themeName].push(caseItem);
                 }
+            }else{
+                if (groupedCases(caseItem.themeName)=== this.prefilledTheme){
+                    groupedCases[caseItem.themeName].push(caseItem);
+                }else {
+                    groupedCases[caseItem.themeName] = [];
+                    groupedCases[caseItem.themeName].push(caseItem);
+                }
+            }
         });
 
         for (const [key, value] of Object.entries(groupedCases)) {
