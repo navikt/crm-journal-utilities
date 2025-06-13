75f49bc4ed1552872b37dfbb8bb226021d89e43d
saramoh2309 <86772218+saramoh2309@users.noreply.github.com>
2025-06-13 14:21:56 +0200

    added new template for nav case item

--- force-app/main/default/lwc/crmNavCaseItem/crmNavCaseItem.js
+++ force-app/main/default/lwc/crmNavCaseItem/crmNavCaseItem.js
@@ -1,4 +1,7 @@
 import { LightningElement, api } from 'lwc';
+import originalTemplate from './crmNavCaseItem.html';
+import newDesignTemplate from './crmNavCaseItemNewDesign.html';
+import sharedStyling from './sharedStyling.css';
 
 export default class NksNavCaseItem extends LightningElement {
     @api navCase;
@@ -6,6 +9,12 @@ export default class NksNavCaseItem extends LightningElement {
     @api index;
     @api isLast;
     @api useNewDesign = false;
+
+    static stylesheets = [sharedStyling];
+
+    render() {
+        return this.useNewDesign ? newDesignTemplate : originalTemplate;
+    }
 
     caseSelected() {
         let selectedCase = this.navCase;
@@ -21,23 +30,21 @@ export default class NksNavCaseItem extends LightningElement {
     }
 
     get className() {
-        let classNames =
-            (this.useNewDesign ? 'slds-var-p-vertical_x-small ' : 'slds-var-p-vertical_xx-small ') + 'case-tile-bg ';
-
-        if (this.isLast) {
-            classNames += 'item-last ';
-        }
-
-        classNames += this.selected ? 'selected' : this.index % 2 === 1 ? 'item-grey' : '';
-
-        return classNames;
+        return [
+            this.useNewDesign ? 'slds-var-p-vertical_x-small' : 'slds-var-p-vertical_xx-small',
+            'case-tile-bg',
+            this.isLast ? 'item-last' : this.useNewDesign ? 'slds-border_bottom' : '',
+            !this.useNewDesign && this.selected
+                ? 'selected'
+                : !this.useNewDesign && this.index % 2 === 1
+                  ? 'item-grey'
+                  : ''
+        ]
+            .filter(Boolean)
+            .join(' ');
     }
 
     get isClosed() {
         return this.navCase ? this.navCase.lukket : false;
-    }
-
-    get itemClass() {
-        return this.useNewDesign ? 'custom-padding' : 'slds-var-p-left_medium';
     }
 }
