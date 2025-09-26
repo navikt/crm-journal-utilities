81d7982efc4c64a0c933ce3ba1113ad75e5b815b
eirikfladby <eirik.fladby@gmail.com>
2025-09-26 14:55:35 +0200

    remove v1

--- force-app/main/default/lwc/crmNavCaseItem/crmNavCaseItem.js
+++ force-app/main/default/lwc/crmNavCaseItem/crmNavCaseItem.js
@@ -1,20 +1,10 @@
 import { LightningElement, api } from 'lwc';
-import originalTemplate from './crmNavCaseItem.html';
-import newDesignTemplate from './crmNavCaseItemNewDesign.html';
-import sharedStyling from './sharedStyling.css';
 
 export default class NksNavCaseItem extends LightningElement {
     @api navCase;
     @api selected = false; //Attribute set by parent
     @api index;
     @api isLast;
-    @api useNewDesign = false;
-
-    static stylesheets = [sharedStyling];
-
-    render() {
-        return this.useNewDesign ? newDesignTemplate : originalTemplate;
-    }
 
     caseSelected() {
         let selectedCase = this.navCase;
@@ -31,14 +21,10 @@ export default class NksNavCaseItem extends LightningElement {
 
     get className() {
         return [
-            this.useNewDesign ? 'slds-var-p-vertical_x-small' : 'slds-var-p-vertical_xx-small',
+            'slds-var-p-vertical_x-small',
             'case-tile-bg',
-            this.isLast ? 'item-last' : this.useNewDesign ? 'slds-border_bottom' : '',
-            !this.useNewDesign && this.selected
-                ? 'selected'
-                : !this.useNewDesign && this.index % 2 === 1
-                  ? 'item-grey'
-                  : ''
+            this.isLast ? 'item-last' : 'slds-border_bottom',
+            ''
         ]
             .filter(Boolean)
             .join(' ');
